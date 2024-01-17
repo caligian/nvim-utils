f0f44e23582349db06514679da2c5e19ac21668f
caligian <caligian@protonmail.com>
2024-01-18 00:46:56 +0530

    modularizing filetype to prevent fucking bugs in the future

---
+++ nvim-utils/Filetype/commands.lua
@@ -0,0 +1,236 @@
+-- local utils = require 'nvim-utils.Filetype.utils'
+local utils = loadfile "utils.lua"()
+local mod = {}
+
+--------------------------------------------------
+--- @alias command string | function | ({[1]: any, [2]: function})[]
+
+--- @class Command
+--- @field buffer? command
+--- @field workspace? command
+--- @field dir? command
+--- @field root_dir? string[] | string workspace_root_patterns
+
+--- @class REPLCommand : Command
+--- @field on_input? function
+--- @field load_from_path? function
+
+--- @class FormatCommand : Command
+--- @field stdin? boolean
+
+--- @param p string path
+--- @param spec Command | command
+--- @return {[1]: string, [2]: string}
+local function match_command(p, spec)
+  --[[
+  lookup spec:
+  eg: {{is_string, string.upper}, {'^[0-9]+$', tonumber}, ...}
+
+  {<test>, <function>},
+  <test> = <function> | <lua pattern>
+  --]]
+
+  local lookup_dict = function(x)
+    local ok = is_table(x) and list.is_a(x, function(X)
+      return #X == 2 and is_callable(X[2])
+    end)
+
+    if not ok then
+      return false, ("expected {<test>, <callable>}, got " .. dump(x))
+    end
+
+    return true
+  end
+
+  local switch = case {
+    {
+      lookup_dict,
+      function(obj)
+        for i = 1, #obj do
+          local test, fun = unpack(obj[i])
+          local ok
+
+          if is_string(test) then
+            if p:match(test) then
+              ok = true
+            end
+          elseif test(p) then
+            ok = true
+          end
+
+          if ok then
+            return { p, fun(p) }
+          end
+        end
+      end,
+    },
+    {
+      is_function,
+      function(f)
+        return { p, f(p) }
+      end,
+    },
+    {
+      is_string,
+      function(s)
+        return { p, s }
+      end,
+    },
+  }
+
+  local ok = switch:match(spec)
+  if not ok then
+    error("invalid command spec " .. dump(spec))
+  end
+
+  return ok
+end
+
+local function is_command(spec)
+  if is_string(spec) then
+    return true
+  end
+
+  local ok = dict.has_some_keys(spec, { "buffer", "workspace", "dir" })
+  if not ok then
+    return false, "expected dict to have any of .buffer, .workspace, .dir, got " .. dump(spec)
+  end
+
+  return true
+end
+
+--- @class get_command_return
+--- @field workspace? {[1]: string, [2]: string}
+--- @field buffer? {[1]: string, [2]: string}
+--- @field dir? {[1]: string, [2]: string}
+
+--- @param bufnr number
+--- @param spec Command | command
+--- @return get_command_return?, string?
+local function get_command(bufnr, spec)
+  if not is_command(spec) then
+    local msg = [[expected string | {{test, callback}, ...}, got ]] .. dump(x)
+    return nil, msg
+  elseif is_string(spec) then
+    spec = { buffer = spec }
+  end
+
+  local res = {}
+  if spec.workspace then
+    local ws_pat = spec.root_dir
+    local ws = utils.get_workspace(bufnr, ws_pat, 4)
+
+    if ws then
+      res.workspace = match_command(ws, spec.workspace)
+    end
+  end
+
+  if spec.dir then
+    local dirname = Path.dirname(Buffer.get_name(bufnr))
+
+    ---@diagnostic disable-next-line: param-type-mismatch
+    res.dir = match_command(dirname, spec.dir)
+  end
+
+  if spec.buffer then
+    res.buffer = match_command(Buffer.get_name(bufnr), spec.buffer)
+  end
+
+  list.each(keys(res), function(k)
+    local v = res[k]
+    local p, cmd = unpack(v)
+    local templ = template(cmd, { path = p })
+
+    if templ then
+      res[k] = templ
+    end
+  end)
+
+  return res
+end
+
+--- @param cmd_type "repl" | "compile" | "build" | "test" | "format"
+--- @param cmd command | Command
+local function validate(cmd_type, cmd)
+  assert(strmatch(cmd_type, "repl", "compile", "build", "test", "format"))
+
+  local sig = union("string", "function", "table")
+  local common = {
+    __extra = true,
+    ["buffer?"] = sig,
+    ["workspace?"] = sig,
+    ["dir?"] = sig,
+  }
+  local validators = {
+    repl = {
+      __extra = true,
+      ["on_input?"] = "function",
+      ["load_from_path?"] = "function",
+    },
+    formatter = {
+      __extra = true,
+      ["stdin?"] = "boolean",
+    },
+  }
+
+  params { command = { common, cmd } }
+
+  local test = validators[cmd_type]
+  if test then
+    params { command = { test, cmd } }
+  end
+
+  assert(is_command(cmd))
+
+  return cmd
+end
+
+--- @param bufnr number
+--- @param cmd_type "repl" | "compile" | "build" | "test" | "format"
+--- @param spec command | Command
+--- @param cmd_for string "buffer" | "workspace" | "dir"
+--- @return (string|get_command_return)?
+function mod.get_command(bufnr, cmd_type, spec, cmd_for)
+  validate(cmd_type, spec)
+
+  spec = is_string(spec) and { buffer = spec } or spec
+
+  if cmd_for then
+    assert(spec[cmd_for], "expected any of buffer, workspace, dir, got " .. dump(cmd_for))
+
+    spec = { [cmd_for] = spec[cmd_for] }
+    local ok = get_command(bufnr, spec)
+
+    if ok then
+      return ok[cmd_for]
+    end
+  end
+
+  return get_command(bufnr, spec)
+end
+
+--- @param spec command | Command
+--- @return table?
+function mod.get_opts(spec)
+  spec = is_string(spec) and { buffer = spec } or spec
+  return dict.filter_unless(spec --[[@as table]], function(key, _)
+    return strmatch(key, "^buffer$", "^workspace$", "^dir$")
+  end)
+end
+
+--- @param bufnr number
+--- @param cmd_type "repl" | "compile" | "build" | "test" | "format"
+--- @param spec command | Command
+--- @param cmd_for string "buffer" | "workspace" | "dir"
+--- @return (string|get_command_return)?, table?
+function mod.get_command_and_opts(bufnr, cmd_type, spec, cmd_for)
+  local cmd = mod.get_command(bufnr, cmd_type, spec, cmd_for)
+  if not cmd then
+    return
+  end
+
+  local opts = mod.get_opts(spec) or {}
+  return cmd, opts
+end
+
+return cmd
---
+++ nvim-utils/Filetype/utils.lua
@@ -0,0 +1,89 @@
+local utils = {}
+local buf = Buffer
+local path = Path
+
+function utils.resolve(name)
+  assert_is_a(name, union('Filetype', 'string'))
+  if typeof(name) == 'Filetype' then
+    return name
+  end
+
+  return user.filetypes[name]
+end
+
+function utils.query(ft, attrib, f)
+  local obj = utils.resolve(ft)
+  if not obj then
+    return 'invalid filetype ' .. dump(ft)
+  end
+
+  obj = dict.get(obj, totable(attrib))
+  if not obj then
+    return string.format('%s: invalid attribute: %s', dump(ft), dump(attrib))
+  end
+
+  if f then
+    return f(obj)
+  end
+
+  return obj
+end
+
+function utils.find_workspace(start_dir, pats, maxdepth, _depth)
+  maxdepth = maxdepth or 5
+  _depth = _depth or 0
+  pats = totable(pats or "%.git$")
+
+  if maxdepth == _depth then
+    return false
+  end
+
+  if not path.is_dir(start_dir) then
+    return false
+  end
+
+  local parent = path.dirname(start_dir)
+  local children = path.getfiles(start_dir)
+
+  for i = 1, #pats do
+    local pat = pats[i]
+
+    for j = 1, #children do
+      if children[j]:match(pat) then
+        return children[j]
+      end
+    end
+  end
+
+  return find_workspace(parent, pats, maxdepth, _depth + 1)
+end
+
+function utils.get_workspace(bufnr, pats, maxdepth, _depth)
+  if not buf.exists(bufnr) then
+    return
+  end
+
+  local bufname = buf.get_name(bufnr)
+  local server = utils.query(buf.filetype(bufnr), "server")
+
+  if server then
+    local lspconfig = require "lspconfig"
+    server = totable(server)
+
+    local config = is_string(server) and lspconfig[server] or lspconfig[server[1]]
+
+    local root_dir_checker = server.get_root_dir
+      or config.document_config.default_config.root_dir
+      or config.get_root_dir
+
+    if root_dir_checker then
+      return root_dir_checker(bufname)
+    else
+      return find_workspace(bufname, pats, maxdepth, _depth)
+    end
+  else
+    return find_workspace(bufname, pats, maxdepth, _depth)
+  end
+end
+
+return utils
